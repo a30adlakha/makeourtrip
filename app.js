5fc005c3f0b30f3fad041d87ff192be97b79abb9
AyushAjay14 <ayushajay52@gmail.com>
2022-06-30 03:08:39 +0530

    changes

--- app.js
+++ app.js
@@ -109,12 +109,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-let port = process.env.PORT;
-if(port == null || port == "")
-{
-  port=3000;
-}
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('serving on port 3000');
 })
 
