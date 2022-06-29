f0ac25a3ec27ebdc0fce22a831274ac0a8e1ccbf
AbhinavAdlakha <abhinavadlakha@Abhinavs-MacBook-Pro.local>
2022-06-30 00:24:48 +0530

    intial comment

---
+++ seeds/cities.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        city: "The suryaa",
+        growth_from_2000_to_2013: "0.1%",
+        latitude: 28.56134,
+        longitude: 77.2693,
+        rank: "1",
+        state: "New Delhi",
+        image1: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+        image2:  "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png"
+    },
+    {
+        city: "The Leela",
+        growth_from_2000_to_2013: "0.1%",
+        latitude: 28.5799,
+        longitude: 77.1893,
+        rank: "2",
+        state: "New Delhi",
+        image1: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+        image2:  "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png"
+    },
+    {
+        city: "Vivanta",
+        growth_from_2000_to_2013: "0.1%",
+        latitude: 28.5581,
+        longitude: 77.0627,
+        rank: "3",
+        state: "New Delhi",
+        image1: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+        image2:  "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png"
+    },
+    {
+        city: "Radisson Blu",
+        growth_from_2000_to_2013: "0.1%",
+        latitude: 28.6139,
+        longitude: 77.2090,
+        rank: "4",
+        state: "New Delhi",
+        image1: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+        image2:  "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png"
+    },
+    {
+        city: "The Lalit",
+        growth_from_2000_to_2013: "0.1%",
+        latitude: 28.6317,
+        longitude: 77.2272,
+        rank: "5",
+        state: "New Delhi",
+        image1: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+        image2:  "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png"
+    },
+];
